7fd2de9ee697762cd562d36a3f24164a62246164
Russ <russellcaughey@gmail.com>
2015-07-14 12:07:21 -0700

    Added credits

---
+++ Assets/Credits.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Credits : MonoBehaviour {
+
+	public string[] messages;
+	public float timeBetweenMessages = 3f;
+
+	private Text currentMessage;
+	private int counter;
+	private float timer;
+
+	void Awake(){
+		currentMessage = GetComponent<Text>();
+	}
+		
+	void Start(){
+		timer = timeBetweenMessages;
+		currentMessage.text = messages[0];
+	}
+
+	void Update(){
+		if(timer >= 0){
+			timer -= Time.deltaTime;
+		}
+
+		if(timer <= 0 && counter+1 < messages.Length){
+			DisplayNextMessage();
+		}
+	}
+
+	void DisplayNextMessage(){
+		counter++;
+		currentMessage.text = messages[counter];
+		timer = timeBetweenMessages;
+	}
+}
