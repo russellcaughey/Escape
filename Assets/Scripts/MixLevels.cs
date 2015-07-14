97b4651bb23457bd4d8a26aef976f815d27a291f
Russ <russellcaughey@gmail.com>
2015-07-13 20:33:57 -0700

    Persistent music through scenes

--- Assets/Scripts/MixLevels.cs
+++ Assets/Scripts/MixLevels.cs
@@ -6,6 +6,10 @@ public class MixLevels : MonoBehaviour {
 
 	public AudioMixer masterMixer;
 
+	void Awake(){
+		DontDestroyOnLoad(transform.gameObject);
+	}
+
 	public void SetSfxLevel(float sfxLevel){
 		masterMixer.SetFloat("sfxVol", sfxLevel);
 		masterMixer.SetFloat("sfxVol", sfxLevel);
