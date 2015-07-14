bc82a9b00908c110ff8399812c320bbd8378287a
Russ <russellcaughey@gmail.com>
2015-07-14 15:07:46 -0700

    Added opening scene

---
+++ Assets/StartGameTrigger.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StartGameTrigger : MonoBehaviour {
+	
+	public string startLevel;
+	public Door startDoor;
+
+	void OnTriggerEnter(){
+		StartCoroutine("StartGame");
+	}
+
+	IEnumerator StartGame(){
+		startDoor.Close();
+		yield return new WaitForSeconds(1);
+		Application.LoadLevel(startLevel);
+	}
+}
